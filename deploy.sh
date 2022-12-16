c33c10f9aae346d3155dcb92889a9f23aaaed556
Vitor HP Bittencourt <vitorhp2@gmail.com>
2022-12-16 08:13:04 -0300

    deploy

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+npm run build
+# navigate into the build output directory
+mv dist docs
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:vitorhp/blog-ui.git master
